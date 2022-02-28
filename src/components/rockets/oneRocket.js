61eec10cd22176df4f70d9b7c306c7237998aeb5
VuDej <drvu47@gmail.com>
2022-03-01 00:02:45 +0100

    added action for booking Rocket in redux file and OneROcket.js

--- src/components/rockets/oneRocket.js
+++ src/components/rockets/oneRocket.js
@@ -1,32 +1,49 @@
+import { useDispatch } from 'react-redux';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { bookingRocket } from '../../redux/rockets/rockets';
 import './oneRocket.css';
 
-const OneRocket = ({ prop }) => {
+const OneRocket = ({ rocket }) => {
   const {
 
     id,
     rocketName,
     description,
     flickrImages,
-  } = prop;
+    reserved,
+  } = rocket;
+
+  const dispatch = useDispatch();
+
+  const booking = (id) => {
+    dispatch(bookingRocket(id));
+  };
 
   return (
     <div className="rocket" id={id}>
       <img src={flickrImages} alt="" />
       <div className="column">
+
         <h2 className="rocket-name">{rocketName}</h2>
         <p className="rocket-description">{description}</p>
-        <button type="button" className="button">Reserve</button>
+        {!reserved && (
+        <button type="button" onClick={() => booking(id)} className="booking-button">Reserve Rocket</button>
+        )}
       </div>
     </div>
   );
 };
 
+OneRocket.defaultProps = {
+  reserved: false,
+};
+
 OneRocket.propTypes = {
   rocketName: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  prop: PropTypes.arrayOf.isRequired,
+  rocket: PropTypes.func.isRequired,
+  reserved: PropTypes.bool,
 };
 
 export default OneRocket;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,4 +1,5 @@
 const GET_ROCKETS = 'rocketStore/rockets/GET_ROCKETS';
+const RESERVE_ROCKET = 'rocketStore/rockets/RESERVE_ROCKET';
 
 const initialState = [];
 
@@ -7,10 +8,26 @@ export const getRockets = (payload) => ({
   payload,
 });
 
+export const bookingRocket = (payload) => ({
+  type: RESERVE_ROCKET,
+  payload,
+});
+
 const reducerRockets = (state = initialState, action) => {
   switch (action.type) {
     case GET_ROCKETS:
       return [...action.payload];
+    case RESERVE_ROCKET: {
+      const newState = state.map((rocket) => {
+        if (rocket.id !== action.payload) {
+          return rocket;
+        }
+
+        return { ...rocket, reserved: true };
+      });
+
+      return newState;
+    }
     default:
       return state;
   }
